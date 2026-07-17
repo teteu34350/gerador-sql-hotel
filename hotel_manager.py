f045fbf42f7a664fd38f2a90c0cd6486b78f37f1
Matheus <teteu34350@gmail.com>
2026-07-17 12:34:12 -0300

    Na hotel_manager foi adicionada funções que busca nome dos hotéis e busca seu ID pelo nome

--- hotel_manager.py
+++ hotel_manager.py
@@ -4,7 +4,19 @@ def carregar_hoteis():
     with open('hotels.json', 'r', encoding='utf-8') as arquivo:
         dados = json.load(arquivo)
         return dados    
+def listar_nomes():
+     hoteis = carregar_hoteis()
+     nome  = []
+     for hotel in hoteis:
+         nome.append(hotel['Nome'])
+     return nome
+def buscar_id(nome_hotel):
+    hoteis  = carregar_hoteis()
+    for hotel in hoteis:
+        if hotel['Nome'] == nome_hotel:
+            return hotel['ID_hotel']
+    return None
 
-
-hoteis  = carregar_hoteis()
-print(hoteis)
+print(carregar_hoteis())
+print(listar_nomes())
+print(buscar_id('Máceio Mar RESORT'))
